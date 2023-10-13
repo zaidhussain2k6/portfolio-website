a1f759a6a74ffe0d34c0a155de41f8d3552701b1
zaidhussain2k6 <zaidhussain2k6@gmail.com>
2023-10-13 08:55:40 +0500

    Added AboutMeSec Component Section

--- src/components/AboutMeSec.jsx
+++ src/components/AboutMeSec.jsx
@@ -7,7 +7,7 @@ import AboutMe from '../images/AboutMe.svg'
 
 function AboutMeSec() {
   return (
-    <div className='flex-wrap px-16 py-36 inline-flex items-center justify-between gap-36'>
+    <div className='flex-wrap px-24 xs:gap-12 lg:gap-16 py-36 flex items-center justify-between md:gap-28'>
 
     <div className='py-24 items-center flex gap-12'>
       <div>
@@ -24,8 +24,8 @@ function AboutMeSec() {
 
     
           <div className='space-y-6'>
-            <h3 className='font-FontA font-semibold text-3xl text-darkblue'>UI/UX Designer & Frontend Developer</h3>
-            <p className='font-medium text-xl text-gray w-auto max-w-[820px] font-FontB'>I'm a passionate Designer and Developer hailing from Pakistan. With a profound love for coding, I find immense joy in crafting visually appealing and functional digital experiences. Let's create something remarkable together! 💻📱🚀</p>
+            <h3 className='font-FontA font-semibold lg:text-4xl xl:text-6xl text-3xl text-darkblue'>UI/UX Designer & Frontend Developer</h3>
+            <p className='font-medium xl:text-2xl text-xl text-gray w-auto max-w-[820px] xl:max-w-[1200px] font-FontB'>I'm a passionate Designer and Developer hailing from Pakistan. With a profound love for coding, I find immense joy in crafting visually appealing and functional digital experiences. Let's create something remarkable together! 💻📱🚀</p>
           </div>
 
     </div>
